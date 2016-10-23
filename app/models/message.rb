22ab7ca10f50e20045ac6390cce56ca93d073910
Riley Trautman <asonix.dev@gmail.com>
2016-10-23 02:58:42 -0500

    add timestamp to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message
   include Mongoid::Document
   field :body, type: String
+  field :sent, type: DateTime
+
   embedded_in :user, inverse_of: :to
   embedded_in :user, inverse_of: :from
 end
